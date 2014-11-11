63e0b5a1b039eb03b8e296cd90e90f8e4e75b6b6
Evan Myller <eMyller@7ws.co>
2014-11-11 00:39:31 -0200

    feat(acc): Introduce a bare Signup page

---
+++ brasilcomvc/accounts/tests/test_views.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from brasilcomvc.common.views import AnonymousRequiredMixin
+
+from ..views import Signup
+
+User = get_user_model()
+
+
+class SignupTestCase(TestCase):
+
+    url = reverse('signup')
+
+    def test_signup_page_is_accessible(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_inherits_anonymous_required_mixin(self):
+        self.assertTrue(issubclass(Signup, AnonymousRequiredMixin))
+
+    def test_signup_success(self):
+        user_data = {
+            'email': 'user@example.com',
+            'full_name': 'Test User',
+            'password': '123',
+        }
+        response = self.client.post(self.url, user_data)
+        self.assertRedirects(response, self.url)
+        self.assertTrue(User.objects.filter(email=user_data['email']).exists())
+
+    def test_signup_failure(self):
+        response = self.client.post(self.url, {})
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.exists())
+        self.assertEqual(
+            set(response.context['form'].errors),
+            set(['email', 'full_name', 'password']))
--- brasilcomvc/urls.py
+++ brasilcomvc/urls.py
@@ -6,4 +6,6 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'', include('brasilcomvc.accounts.urls')),
 ]
