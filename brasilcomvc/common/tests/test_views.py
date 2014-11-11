f5f3a5c245cb3dfa9fe9bbd5b113684bd11f1aee
Evan Myller <eMyller@7ws.co>
2014-11-11 15:12:18 -0200

    feat(common): Add an "anonymous only" view mixin

--- brasilcomvc/common/tests/test_views.py
+++ brasilcomvc/common/tests/test_views.py
@@ -5,13 +5,19 @@ from django.http import HttpResponse
 from django.test import RequestFactory, TestCase
 from django.views.generic import View
 
-from ..views import LoginRequiredMixin
+from ..views import AnonymousRequiredMixin, LoginRequiredMixin
 
 
 User = get_user_model()
 
 
 class RequiresLogin(LoginRequiredMixin, View):
+
+    def get(self, *args, **kwargs):
+        return HttpResponse('OK')
+
+
+class RequiresAnonymous(AnonymousRequiredMixin, View):
 
     def get(self, *args, **kwargs):
         return HttpResponse('OK')
@@ -35,3 +41,23 @@ class LoginRequiredMixinTestCase(TestCase):
         response = view(request)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'OK')
+
+
+class AnonymousRequiredMixinTestCase(TestCase):
+
+    def test_anonymous_user_should_access_protected_view(self):
+        view = RequiresAnonymous.as_view()
+        request = RequestFactory().get('/')
+        request.user = AnonymousUser()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'OK')
+
+    def test_authenticated_user_should_be_redirected_to_login_redirect_url(
+            self):
+        view = RequiresAnonymous.as_view()
+        request = RequestFactory().get('/')
+        request.user = User()
+        response = view(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], settings.LOGIN_REDIRECT_URL)
--- brasilcomvc/common/views.py
+++ brasilcomvc/common/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 
 
@@ -11,3 +13,16 @@ class LoginRequiredMixin(object):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+
+
+class AnonymousRequiredMixin(object):
+
+    '''
+    Make any view be accessible by unauthenticated users only
+    '''
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return super(AnonymousRequiredMixin, self).dispatch(
+            request, *args, **kwargs)
