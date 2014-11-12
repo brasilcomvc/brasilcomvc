c7f6e0c2e9c5be112a7576c3d2a1fc8a79eb9f18
Evan Myller <eMyller@7ws.co>
2014-11-12 14:29:05 -0200

    fix(set): Fix django-pipeline configuration for development/test

--- brasilcomvc/settings/staticfiles.py
+++ brasilcomvc/settings/staticfiles.py
@@ -2,9 +2,6 @@ import os
 import sys
 
 
-# Disable django-pipeline when in test mode
-PIPELINE_ENABLED = 'test' not in sys.argv
-
 # Main project directory
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 STATIC_BASE_DIR = os.path.join(BASE_DIR, '../webroot')
@@ -18,7 +15,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # django-pipeline settings
+
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+if 'test' in sys.argv:
+    STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineStorage'
+
 STATICFILES_FINDERS = (
     'pipeline.finders.AppDirectoriesFinder',
     'pipeline.finders.PipelineFinder',
