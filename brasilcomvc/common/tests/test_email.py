472bf69d7e81c3483e700af16339c81033f99d6f
Evan Myller <eMyller@7ws.co>
2014-11-18 14:49:28 -0200

    fix(common): Split email tests for each scenario

--- brasilcomvc/common/tests/test_email.py
+++ brasilcomvc/common/tests/test_email.py
@@ -28,18 +28,19 @@ class TemplateEmailTestCase(EmailTestMixin, TestCase):
         self.only_email_sent('test')
         self.assertEqual(mail.outbox[0].to[0], 'user@localhost')
 
-    def test_email_should_render_plain_text_and_html_correctly(self):
+    def test_email_should_render_plain_text_correctly(self):
         timestamp = str(datetime.now())
         send_template_email(
             'test', 'user@localhost', 'test_email.html', {'value': timestamp})
         email = mail.outbox[-1]
-        expected_html = '<span data-test>{}</span>'.format(timestamp)
-
-        # Value is found in plain text body
-        self.assertNotIn(expected_html, email.body)
+        self.assertNotIn('<span', email.body)
         self.assertIn(timestamp, email.body)
 
-        # Value (with markup) is found in HTML body
+    def test_email_should_render_html_correctly(self):
+        timestamp = str(datetime.now())
+        send_template_email(
+            'test', 'user@localhost', 'test_email.html', {'value': timestamp})
+        email = mail.outbox[-1]
         self.assertEqual(len(email.alternatives), 1)
         html, mimetype = email.alternatives[0]
-        self.assertIn(expected_html, html)
+        self.assertIn('<span data-test>{}</span>'.format(timestamp), html)
