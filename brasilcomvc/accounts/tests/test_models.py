770d2a674e2ca92e7506e5a173c4b890a3343d43
Evan Myller <eMyller@7ws.co>
2014-12-02 15:12:11 -0200

    feat(accounts): add tests for the User manager

--- brasilcomvc/accounts/tests/test_models.py
+++ brasilcomvc/accounts/tests/test_models.py
@@ -3,6 +3,30 @@ from django.test import TestCase
 from ..models import User
 
 
+class UserManagerTestCase(TestCase):
+
+    _user_data = {
+        'email': 'test@example.com',
+        'password': '123',
+        'full_name': 'Test User',
+    }
+
+    def test_create_user(self):
+        data = self._user_data.copy()
+        self.assertFalse(User.objects.exists())
+        User.objects.create_user(**data)
+        raw_pwd = data.pop('password')
+        self.assertTrue(User.objects.filter(**data).exists())
+        user = User.objects.all()[0]
+        self.assertTrue(user.check_password(raw_pwd))
+
+    def test_create_superuser(self):
+        User.objects.create_superuser(**self._user_data)
+        user = User.objects.all()[0]
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+
 class UserTestCase(TestCase):
 
     def test_short_name_with_long_name(self):
