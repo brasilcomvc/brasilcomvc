b705c5bc5ef8b2ad97bb28b5b9b49591dfc4f682
Evan Myller <eMyller@7ws.co>
2014-12-10 15:18:34 -0200

    fix(projects): save a Project's slug once only

--- brasilcomvc/projects/tests/test_models.py
+++ brasilcomvc/projects/tests/test_models.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from ..models import Project
+
+User = get_user_model()
 
 
 class ProjectTestCase(TestCase):
 
     def test_project_slug_gets_generated_correctly(self):
-        project = Project(name='This is a Test Project')
-        project.clean()
+        project = Project(
+            owner=User.objects.create_user('test@example.com', '123'),
+            name='This is a Test Project',
+        )
+        project.save()
         self.assertEqual(project.slug, 'this-is-a-test-project')
+        old_slug = project.slug
+        project.name = 'Some New Name'
+        project.save()
+        self.assertEqual(project.slug, old_slug)
