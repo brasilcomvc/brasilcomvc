d78c4aaa393fbc215bc8bf2170556ac835e6a64c
Rodrigo Chacon <rochacon@gmail.com>
2014-12-11 15:20:42 -0200

    fix(projects): make agenda field default= and not null

--- brasilcomvc/projects/migrations/0007_project_agenda.py
+++ brasilcomvc/projects/migrations/0007_project_agenda.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='project',
             name='agenda',
-            field=models.TextField(null=True, blank=True),
+            field=models.TextField(default='', blank=True),
             preserve_default=True,
         ),
     ]
