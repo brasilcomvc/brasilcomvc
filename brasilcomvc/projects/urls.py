fe112229194f1372d40d1bf0b3cc87e9a115e52b
Evan Myller <eMyller@7ws.co>
2014-12-11 16:41:47 -0200

    feat(projects): introduce the [still very basic] Project List

--- brasilcomvc/projects/urls.py
+++ brasilcomvc/projects/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import url
 
 from .views import (
     ProjectDetails,
+    ProjectList,
 )
 
 
 urlpatterns = (
+    # Project List
+    url(r'^$',
+        ProjectList.as_view(), name='project_list'),
+
     # Project Details
     url(r'^(?P<slug>[\w-]+)/$',
         ProjectDetails.as_view(), name='project_details'),
