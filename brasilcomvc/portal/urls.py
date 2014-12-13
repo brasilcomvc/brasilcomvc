fe0404815e5d75240f8603b7abb58a219842e03a
Evan Myller <eMyller@7ws.co>
2014-12-12 22:07:25 -0200

    feat(portal): add the About page
    Closes #79

--- brasilcomvc/portal/urls.py
+++ brasilcomvc/portal/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    # About
+    url(r'^sobre/$',
+        TemplateView.as_view(template_name='portal/about.html'), name='about'),
 ]
