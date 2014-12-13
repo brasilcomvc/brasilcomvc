ebb41df78bc6a648e80eb39a2bd3974d63dcb337
Evan Myller <eMyller@7ws.co>
2014-12-12 22:14:21 -0200

    feat(portal): add the How It Works page

--- brasilcomvc/portal/urls.py
+++ brasilcomvc/portal/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     # About
     url(r'^sobre/$',
         TemplateView.as_view(template_name='portal/about.html'), name='about'),
+
+    # How It Works
+    url(r'^como-funciona/$',
+        TemplateView.as_view(template_name='portal/how_it_works.html'),
+        name='how_it_works'),
 ]
