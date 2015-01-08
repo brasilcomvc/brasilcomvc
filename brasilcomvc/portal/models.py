f8c0c61b01ea38f350a8857115b1634183b9503d
Evan Myller <eMyller@7ws.co>
2015-01-08 16:36:34 -0200

    feat(portal): add text for humans @ Home banners

--- brasilcomvc/portal/models.py
+++ brasilcomvc/portal/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.template.defaultfilters import striptags, truncatechars
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
@@ -10,6 +12,7 @@ def project_img_upload_to(instance, filename):
     return 'homebanners/{}/image.jpeg'.format(instance.id)
 
 
+@python_2_unicode_compatible
 class HomeBanner(models.Model):
 
     image = ProcessedImageField(
@@ -23,3 +26,10 @@ class HomeBanner(models.Model):
     image.help_text = 'Imagem de alta resolução; será cortada para 1400x550.'
     content.verbose_name = 'conteúdo'
     content.help_text = 'Conteúdo (HTML) para sobrepor a imagem no banner.'
+
+    class Meta:
+        verbose_name = 'Banner da Home'
+        verbose_name_plural = 'Banners da Home'
+
+    def __str__(self):
+        return truncatechars(striptags(self.content), 40)
