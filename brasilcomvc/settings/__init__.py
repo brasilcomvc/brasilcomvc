c17aefc0205d7d156ca44ff3d3b06c8be830ec63
Rodrigo Chacon <rochacon@gmail.com>
2015-01-08 18:36:15 -0200

    fix(settings): install django.contrib.gis

--- brasilcomvc/settings/__init__.py
+++ brasilcomvc/settings/__init__.py
@@ -42,6 +42,7 @@ DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.gis',
     'django.contrib.humanize',
     'django.contrib.sessions',
     'django.contrib.messages',
