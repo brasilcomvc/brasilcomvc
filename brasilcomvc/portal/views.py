fe7f64d453e36fdd758cfaf529966cf624c22f66
Rodrigo Chacon <rochacon@gmail.com>
2015-04-24 17:02:37 -0300

    portal: Fix blog_redirect settings check

--- brasilcomvc/portal/views.py
+++ brasilcomvc/portal/views.py
@@ -5,7 +5,7 @@ from django.http import Http404, HttpResponsePermanentRedirect
 def blog_redirect(request):
     """Simple redirect to blog URL.
     """
-    if settings.BLOG_URL == '':
+    if not settings.BLOG_URL:
         raise Http404()
     return HttpResponsePermanentRedirect(settings.BLOG_URL)
 
